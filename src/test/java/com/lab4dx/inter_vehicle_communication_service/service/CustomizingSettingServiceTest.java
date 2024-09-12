3277e3454c51cf72a97cb969491432f93f579f9a
Tvivid <tjsaud3250@gmail.com>
2024-09-12 12:39:04 +0900

    message_default.hmtl 수정

--- src/test/java/com/lab4dx/inter_vehicle_communication_service/service/CustomizingSettingServiceTest.java
+++ src/test/java/com/lab4dx/inter_vehicle_communication_service/service/CustomizingSettingServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -12,6 +14,14 @@ public class CustomizingSettingServiceTest {
 
     @Autowired
     private CustomizingSettingService customizingSettingService;
+
+    @Test
+    void getSettingsByMember() {
+        String memberId = "user1";
+        List<CustomizingSetting> expectedSettings = customizingSettingService.getSettingsByMember(memberId);
+        System.out.println(expectedSettings);
+
+    }
 
 //    @Test
 //    public void testInsertSetting() {
