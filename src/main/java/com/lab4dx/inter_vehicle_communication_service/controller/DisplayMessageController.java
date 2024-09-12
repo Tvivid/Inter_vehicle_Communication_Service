f52999cfb28ca7c733b48320fd5d0e78fbe12433
Sunmyeong Jin <tjsaud3250@naver.com>
2024-09-13 04:43:31 +0900

    이미지 출력 추가

--- src/main/java/com/lab4dx/inter_vehicle_communication_service/controller/DisplayMessageController.java
+++ src/main/java/com/lab4dx/inter_vehicle_communication_service/controller/DisplayMessageController.java
@@ -1,5 +1,8 @@
 package com.lab4dx.inter_vehicle_communication_service.controller;
 
+import com.lab4dx.inter_vehicle_communication_service.dto.CustomizingSetting;
+import com.lab4dx.inter_vehicle_communication_service.service.CustomizingSettingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/display")
+@RequestMapping
 public class DisplayMessageController {
-    @GetMapping
+
+    @Autowired
+    private CustomizingSettingService customizingSettingService;
+
+    @GetMapping("/display")
     public String displayMessage(@RequestParam("message") String message, Model model) {
         // 받은 메시지를 모델에 담아서 display.html로 전달
         model.addAttribute("message", message);
         return "display";
     }
+
+    @GetMapping("/display-image")
+    public String displayImage(@RequestParam("customizingId") String customizingId, Model model) {
+
+        String memberId = "user1";
+        CustomizingSetting customizingSetting = customizingSettingService.getSettingByIdAndMemberId(customizingId, memberId);
+
+        model.addAttribute("customizingSetting", customizingSetting);
+        return "image_display";
+    }
+
+
 }
